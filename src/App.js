812d0effc85793a528f73d4c027ff70062595437
eregain <38635276+eregain@users.noreply.github.com>
2023-05-08 16:55:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,6 +20,23 @@ function App() {
       </header>
     </div>
   );
+//calling the function in window.onload to make sure the HTML is loaded
+window.onload = function() {
+	 var pos = 0; 
+	//our box element
+	var box = document.getElementById('img');
+	var t = setInterval(move, 10);
+  
+	function move() {
+		if(pos >= 150) {
+			clearInterval(t);
+		}
+		else {
+			pos += 1;
+			box.style.left = pos+'px';
+		}
+	}
+};
 }
 
 export default App;
